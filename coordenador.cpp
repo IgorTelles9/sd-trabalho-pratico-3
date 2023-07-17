e640d20c62a42e0f28d2ebf6c4626002d6fcc6c9
Igor Telles Barbosa <igortellesbarbosa@ibm.com>
2023-07-17 13:11:51 -0300

    feat(controller): add request tracker

--- coordenador.cpp
+++ coordenador.cpp
@@ -13,17 +13,31 @@
 #include "signal.h"
 #include "getTime.h"
 #include <fstream>
+#include <unordered_map>
 
 std::string IP = "127.0.0.1";
 int PORT = 10000;
 Queue<int> requests;
+std::unordered_map<int, int> requestsTracker;
 Spinlock lock; // used to control the access to the shared variable requests
+
+// Used to write in the log file.
 void logger(Message msg, std::string t){
     std::ofstream log;
     log.open("log.txt", std::ios::app);
     if (log.is_open()) {
         log << t << " - " << msg.text(1) << std::endl; 
         log.close();
+    }
+}
+
+// Used to keep track of how many requests the controller received from each process.
+void tracker(int id){
+    auto proc = requestsTracker.find(id);
+    if (proc != requestsTracker.end()) {
+        proc->second++;
+    } else {
+        requestsTracker[id] = 1;
     }
 }
 
@@ -44,11 +58,12 @@ void* controller(void* arg) {
                 logger(msg, getTime());
                 lock.acquire();
                 if(requests.isEmpty()){
+                    tracker(port); // registers that request was granted
                     lock.release();
                     UDPSender sender(IP, port); // creates sender object
                     Message grant(Message::Type::GRANT, port); // creates GRANT message
                     sender.send(grant.text()); // sends GRANT message
-                    logger(grant, getTime());
+                    logger(grant, getTime()); // logs
                 }
                 lock.release();
                 lock.acquire();
@@ -59,15 +74,16 @@ void* controller(void* arg) {
             if (type == Message::Type::RELEASE){ // process that owns CR's access releases it
                 lock.acquire();
                 if (port == requests.peek()){ // check if the msg came from the process that has the CR
-                    logger(msg, getTime());
+                    logger(msg, getTime()); // logs 
                     requests.pop(); // removes first element of the queue
-                    if (!requests.isEmpty()){
-                       int newPort = requests.peek();
-                       lock.release();
+                    if (!requests.isEmpty()){ // there are processes waiting to use the CR.
+                        int newPort = requests.peek(); // first element will receive access
+                        tracker(newPort); // registers that the request was granted 
+                        lock.release();
                         UDPSender sender(IP, newPort); // creates sender object
                         Message grant(Message::Type::GRANT, newPort); // creates GRANT message
                         sender.send(grant.text()); // sends GRANT message
-                        logger(grant, getTime());
+                        logger(grant, getTime()); // logs 
                     }
                 }
                 lock.release();
@@ -78,20 +94,24 @@ void* controller(void* arg) {
 }
 
 void* monitor(void* arg){
+    std::cout << "1: fila de processos;\n2: lista de atendimentos;\n3: finalizar o programa." << "\n";
+    std::cout << "aguardando..." << "\n";
     while(1){
         int option = 0;
         std::cin >> option;
 
-        if(option == 1){
+        if (option == 1){
             lock.acquire();
-            int proc = requests.peek();
-            std::cout << "Identificador/Porta do processo acessando a região crítica: " << proc << ".\n";
+            std::string procList = requests.getQueue(); 
+            std::cout << "Fila de processos para acesso à região crítica: " << procList << ".\n";
             lock.release();
         }
         else if (option == 2){
             lock.acquire();
-            std::string procList = requests.getQueue(); 
-            std::cout << "Fila de processos para acesso à região crítica: " << procList << ".\n";
+            std::cout << "ID" << "    | "  << " Requests atendidos" <<  "\n"; 
+            for (const auto& entry : requestsTracker) {
+                std::cout << entry.first << " | " << entry.second << "\n";
+            }
             lock.release();
         }
         else if (option == 3){
