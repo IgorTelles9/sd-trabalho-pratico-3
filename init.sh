e179069e83b56966692f6c5ccf8f8f8a0d8204da
Igor Telles Barbosa <igortellesbarbosa@ibm.com>
2023-07-17 16:13:24 -0300

    init commented

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 # Define the range of integers for the loop
 start=1
-end=128
+end=128 # n 
 
 # Loop over the range and run the command
 for ((i=start; i<=end; i++))
 do
-    ./process 10 2 "$i"&
+#             r k  id
+    ./process 3 0 "$i"&
 done
 wait
